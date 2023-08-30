1a23426a1c58f55b3a10cc5e65b98f4451c5cc7c
Sarmad Rehman <sarmadr02@gmail.com>
2023-08-30 08:27:32 -0700

    12-card-component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import { GithubContext } from '../context/context';
-import styled from 'styled-components';
-import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
+import React from "react";
+import { GithubContext } from "../context/context";
+import styled from "styled-components";
+import { MdBusiness, MdLocationOn, MdLink } from "react-icons/md";
 const Card = () => {
-  return <h2>card component</h2>;
+  const { githubUser } = React.useContext(GithubContext);
+  const {
+    avatar_url,
+    html_url,
+    name,
+    company,
+    blog,
+    bio,
+    location,
+    twitter_username,
+  } = githubUser;
+  return <Wrapper>
+    <header>
+      <img src={avatar_url} alt={name}></img>
+        <div>
+      <h4>{name}</h4>
+      <p>@{twitter_username || 'Sarmad Rehman'}</p>
+    </div>
+    <a href={html_url}>follow</a>
+    </header>
+  <p className="bio">{bio}</p>
+  <div className="links">
+    <p><MdBusiness></MdBusiness> {company || 'University of Engineering and Technology Taxila'}</p>
+    <p>
+      <MdLocationOn></MdLocationOn> {location || 'Earth'}
+    </p>
+    <a href={`https://${blog}`}>
+      <MdLink></MdLink> {blog || 'Link for your connection '}
+    </a>
+  </div>
+   </Wrapper>
 };
+
 const Wrapper = styled.article`
   background: var(--clr-white);
   padding: 1.5rem 2rem;
@@ -13,7 +44,7 @@ const Wrapper = styled.article`
   border-bottom-right-radius: var(--radius);
   position: relative;
   &::before {
-    content: 'user';
+    content: "user";
     position: absolute;
     top: 0;
     left: 0;
