51450a9c5b1901b50a664172e9ed2ddc0776d851
Sarmad Rehman <sarmadr02@gmail.com>
2023-09-17 09:39:25 -0700

    32-Promise.allSettled-for

--- src/context/context.js
+++ src/context/context.js
@@ -1,71 +1,92 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com";
 const GithubContext = React.createContext();
 //provider , consumer - GithubContext.Provider
 
-const GithubProvider = ({children }) => {
-    const [githubUser, setGithubUser] = useState(mockUser);
-    const [repos, setRepos] = useState(mockRepos);
-    const [followers, setFollowers] = useState(mockFollowers);
-    //request loading
-    const [requests, setRequests] = useState(0);
-    const [isLoading, setIsLoading] = useState(false);
-    const[error, setError] = useState({show: false, msg:''});
-    const searchGithubUser = async(user) => {
-        toggleError()
-        setIsLoading()
-        const response = await axios(`${rootUrl}/users/${user}`)
-        .catch((err) => 
-            console.log(err)
-        );
-        console.log(response);
-        if (response) {
-            const {login, followers_url}  = response.data;
-            // for repos
-            axios(`${rootUrl}/users/${login}/repos?per_page_100`).then((response)=>
-            setRepos(response.data));
-
-            // https://api.github.com/users/john-smilga/repos?per_page=100)
-            //for followers
-            axios(`${followers_url}/?per_page=100`).then((response)=>   
-            setFollowers(response.data));
-            // (https://api.github.com/users/john-smilga/followers)
-            setGithubUser(response.data);
+const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+  //request loading
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState({ show: false, msg: "" });
+  const searchGithubUser = async (user) => {
+    toggleError();
+    setIsLoading();
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    );
+    
+    if (response) {
+        setGithubUser(response.data);
+      const { login, followers_url } = response.data;
+      // for repos
+      await Promise.allSettled([
+        axios(`${rootUrl}/users/${login}/repos?per_page_100`),
+        axios(`${followers_url}/?per_page=100`),
+      ]).then((results) => {
+        const [ repos, followers ] = results;
+        const status = "fulfilled";
+        if (repos.status === status) {
+          setRepos(repos.value.data);
         }
-        else{  
-            toggleError(true, 'there is no user with that name')
+        if (followers.status === status) {
+          setFollowers(followers.value.data);
+        }
+      }).catch((err)=> console.log(err)); 
+      // https://api.github.com/users/john-smilga/repos?per_page=100)
+      //for followers
+
+      // (https://api.github.com/users/john-smilga/followers)
+    } else {
+      toggleError(true, "there is no user with that name");
     }
     checkRequests();
     setIsLoading(false);
+  };
+  //check rate
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+
+        setRequests(remaining);
+        if (remaining === 0) {
+          toggleError(true, "Sorry, you have exceeded your hourly rate limit");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  //error
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg });
+  }
+
+  useEffect(checkRequests, []);
+  return (
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+        isLoading,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
 };
-    //check rate
-    const checkRequests = () => {
-        axios(`${rootUrl}/rate_limit`).then(({data})=> {
-            let {
-                rate:{remaining},} = data;
-                
-                setRequests(remaining);
-                if(remaining === 0) {
-                    toggleError(true, "Sorry, you have exceeded your hourly rate limit");
-                }
-        }).catch((error) => {console.log(error);});
-    };
-    //error
-    function toggleError(show = false, msg= '') {
-        setError({show, msg});
-    }
-   
-    useEffect(checkRequests,[]);
-    return (
-        <GithubContext.Provider value={{githubUser, repos, followers, 
-        requests, error, searchGithubUser, isLoading}}>
-            {children}
-        </GithubContext.Provider>
-    );
-};
-export {GithubProvider, GithubContext};
+export { GithubProvider, GithubContext };
